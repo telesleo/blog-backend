76a40f557b14c30483b87a2e9f8a90982805156f
Leo <leonardoandrei41@gmail.com>
2023-01-08 21:51:08 -0300

    Modify error middleware to have a generic 500 status code error

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -2,7 +2,11 @@ import { NextFunction, Request, Response } from 'express';
 import AppError from '../errors/appError';
 
 function errorHandler(error: AppError, _req: Request, res: Response, next: NextFunction) {
-  res.status(error.status || 500).send({ message: error.message });
+  if (error.status) {
+    res.status(error.status).json({ message: error.message });
+  } else {
+    res.status(500).send({ message: 'Internal server error.' });
+  }
 
   next();
 }
