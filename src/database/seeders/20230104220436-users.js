f90ec5a833cbcd478ab1cba70692988466db5678
Leo <leonardoandrei41@gmail.com>
2023-01-08 21:48:31 -0300

    Fix hashed password of users from seeder

--- src/database/seeders/20230104220436-users.js
+++ src/database/seeders/20230104220436-users.js
@@ -6,7 +6,7 @@ module.exports = {
         email: 'cezar@email.email',
         username: 'cezar',
         name: 'Cezar A',
-        password: '$2y$10$K0hLAILbILQZmeN4izk9A.JHUSApNTcG2vmGxmtfPeA7YKB6GMICK',
+        password: '$2b$10$GbJ1AZX2wB6Atz1vs1Z/PuKzCTpgMmq.7Uq6a1JVQkRmrO.wvJtyC',
         // password: cezarpassword
         about: 'Hi, my name is Cezar and I am a blogger. I have always had a love for writing and have been fortunate enough to turn my passion into a career. When I\'m not sitting at my computer, you can find me exploring the outdoors or cuddled up with a good book. I specialize in lifestyle and personal development content, and my goal is to inspire and empower others to live their best lives. Thank you for visiting my blog and I hope you find value in the words I share.',
         created_at: '2023-01-06 15:20:05',
@@ -17,7 +17,7 @@ module.exports = {
         email: 'julia@email.email',
         username: 'julia33',
         name: 'Julia C',
-        password: '$2y$10$Cib9ZOoUNoTq5M8fpqAyLu.mFN/6gaBFjQQ2.NyOPU9JvHJvPbRZu',
+        password: '$2b$10$2hxsBvDcUPSHO.zrcCrjie20DRZkW48ifEN71UMJdwCzOL5CIXZHG',
         // password: juliapassword
         about: 'Hello, I am Julia and I\'m a writer. I have always had a love for storytelling and sharing my thoughts and experiences with others. When I\'m not typing away at my computer, you can find me traveling the world or trying out new recipes in the kitchen. My blog covers a variety of topics including travel, food, and personal growth. I hope to inspire and entertain my readers with my stories and insights.',
         created_at: '2023-01-06 18:11:40',
