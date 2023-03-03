74edb485bf3ee4f470d4b29ffca5ac31f0006cd9
leo <leonardoandrei41@gmail.com>
2023-03-03 15:00:51 -0300

    Allow cross-origin requests from a specific origin

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import * as express from 'express';
 import bodyParser = require('body-parser');
 import 'express-async-errors';
@@ -10,6 +11,14 @@ import errorHandler from './middlewares/error.middleware';
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.ORIGIN);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
